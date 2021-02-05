f63558ebabaaebfd9a205d19d2d657eb2c16967e
huliyuan <huliyuan_fe@126.com>
2021-02-05 12:10:56 +0800

    jest

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 export {default as cloneDeep} from './utils/cloneDeep'
+export {default as sum} from './utils/sum'
---
+++ src/utils/sum.js
@@ -0,0 +1,6 @@
+import { func } from "assert-plus";
+
+function sum(a,b) {
+  return a + b
+}
+export default sum
